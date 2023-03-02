49b2296dce162290fcec4b6e7f33fb62eb349aa6
Ihor_Nikolaienko <nikolasha8876@gmail.com>
2023-03-02 21:12:40 +0200

    after added profile card

--- src/components/profileCard/profileCard.tsx
+++ src/components/profileCard/profileCard.tsx
@@ -1,1 +1,21 @@
+import { User } from "../../app/api/api";
 import "./profileCardStyles.scss";
+
+interface Props {
+    userData:User
+}
+
+export const ProfileCard = ({userData}:Props) => {
+    const { name, username, email, phone, website, address, company, id } = userData;
+    return (
+        <div>
+            <p>Ім'я: <span>{name}</span> </p>
+            <p>Нік: <span>{username}</span> </p>
+            <p>Місце роботи: <span>{company.name}</span> </p>
+            <p>Пошта: <span>{email}</span> </p>
+            <p>Телефон: <span>{phone}</span></p>
+            <p>Домашня сторінка: <span>{website}</span> </p>
+            <p>Домашній адрес: <span>{address.zipcode} {address.street} {address.suite} {address.city}</span> </p>
+        </div>
+    );
+};
